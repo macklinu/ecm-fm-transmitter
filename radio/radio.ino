c95a2e84a10f2584cec5a110ba12e7795c1ef47c
Macklin Underdown <macklinu@gmail.com>
2013-03-29 13:18:37 -0400

    Code is all added. Need to start going through it now.

--- radio/radio.ino
+++ radio/radio.ino
@@ -10,20 +10,20 @@
 #include <Wire.h>
 #include <EEPROM.h>
 
-#define topFM  107900000            // Top of the FM Dial Range in USA
-#define botFM   87500000            // Bottom of the FM Dial Range in USA
+#define topFM  107900000            // top of the FM Dial Range in USA
+#define botFM   87500000            // bottom of the FM Dial Range in USA
 #define incrFM    200000            // FM Channel Increment in USA
-#define encoderPinA  2
-#define encoderPinB  3
+#define encoderPinA 2
+#define encoderPinB 3
 #define buttonPin 13
 
-int encoderPos = 0;
-int buttonState;
-int lastButtonState = LOW;
-long lastDebounceTime= 0;
-long debounceDelay = 50;
+int encoderPos = 0; // initial encoder position
+int lastButtonState = LOW; // initial button state
+int buttonState; // current button state
+long lastDebounceTime = 0; // temporary debounce time storage
+long debounceDelay = 50; // debounce time
 
-byte number[] = {
+byte number[] = { // number to be displayed on the 7 segment display
   15, 8, 8, 7};
 byte ls247pins[] = {
   4, 5, 6, 7};
@@ -31,31 +31,46 @@ byte anodepins[] = {
   9, 10, 11, 12};
 byte dp = 8; // decimal point
 
-int serialCount = 0;
+// for parsing incoming serial messages
 int serialArray[2];
+int serialCount = 0;
 
-long frequency = 99300000;          // the default initial frequency in Hz
+long initialFrequency = 99300000;
+long frequency = initialFrequency; // the default initial frequency in Hz
 long newFrequency = 0;
-boolean gOnAir = true;             // Initially, NOT On The Air...
+boolean gOnAir = false; // initially NOT on air
+
+//////////
+// MAIN //
+//////////
 
 void setup() {
-  Serial.begin(9600);                 //for debugging
+  Serial.begin(9600);
   initRadio();
 }
 
-void initRadio() {
-  // Attempt to read the last saved frequency from EEPROM
-  newFrequency = loadFrequency();
-  // Test if outside our FM Range...
-  if ( newFrequency < botFM || newFrequency > topFM ) {
-    // Sorry - haven't saved before - use the default.
-    frequency = 97300000;
+void loop() {
+  readEncoder();
+  // Serial.println( frequency, DEC );
+  print_lcd_frequency(frequency);
+  for (int i=0; i<4; i++) {
+    led7segWriteDigit(i, number[i]);
   }
-  else {
-    // We have a valid frequency!
-    frequency = newFrequency;
-  }
+  checkSerial();
+}
 
+///////////////////
+// OUR FUNCTIONS //
+///////////////////
+
+void initRadio() {
+  // attempt to read the last saved frequency from EEPROM
+  newFrequency = loadFrequency();
+  // test if outside our FM Range...
+  if (newFrequency < botFM || newFrequency > topFM) frequency = initialFrequency; // haven't saved before, use the default.
+  else frequency = newFrequency; // we have a valid frequency!
+
+  // set pins and stuff
   pinMode(encoderPinA, INPUT_PULLUP);
   pinMode(encoderPinB, INPUT_PULLUP);
   pinMode(buttonPin, INPUT_PULLUP);
@@ -66,40 +81,41 @@ void initRadio() {
   pinMode(dp, OUTPUT);  
   digitalWrite(dp, HIGH);
   attachInterrupt(0, doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
+
+  // we're almost there...
   Wire.begin();                       // join i2c bus as master
   transmitter_standby(frequency);
   delay(2000);
   transmitter_setup(frequency);
 }
 
-void loop() {
-  checkSerial();
-}
-
-//////////////////////
-// EEPROM FUNCTIONS //
-//////////////////////
-
-void saveFrequency(long aFrequency) {
-  long memFrequency = 0;                   // For use in Read / Write to EEProm
-
-  //Serial.print( "Saving: " );
-  //Serial.println(aFrequency, DEC);
-
-  memFrequency = aFrequency / 10000;
-  EEPROM.write(0, memFrequency / 256);   // right-most byte
-  EEPROM.write(1, memFrequency - (memFrequency / 256) * 256);   // next to right-most byte
-}
-
-long loadFrequency() {
-  long memFrequency = 0;                   // For use in Read / Write to EEProm
-
-  memFrequency = EEPROM.read(0) * 256 + EEPROM.read(1);
-  memFrequency *= 10000;
-
-  //Serial.print("Retrieving: " );
-  //Serial.println(memFrequency, DEC);
-  return memFrequency;
+void checkSerial() {
+  if (Serial.available() > 0) {
+    int inByte = Serial.read(); // read incoming message
+    // set transmitter into standby mode
+    if (inByte == 254) transmitter_standby(frequency);
+    // change the transmission frequency
+    if (inByte == 255) serialCount = 0;
+    // set incoming byte into a temporary array and move through it
+    // these values will be reassigned
+    else {
+      serialArray[serialCount] = inByte;
+      serialCount++; 
+      if (serialCount == 2) {
+        transmitter_standby(frequency);
+        int left = serialArray[0] * 100;
+        long tempFrequency = left + (long) serialArray[1];
+        frequency = tempFrequency * 10000;
+        set_freq(frequency);
+        saveFrequency(frequency); // save the Frequency to EEPROM Memory
+        // write the incoming messages for debugging in Max
+        Serial.write(serialArray[0]);
+        Serial.write(serialArray[1]);
+        // reset the serial count to receive the next message
+        serialCount = 0;
+      }
+    }
+  }
 }
 
 void readEncoder() {
@@ -144,6 +160,49 @@ void print_lcd_frequency(long input) {
   number[3] = freq;
 }
 
+void led7segWriteDigit(int digit, int value) {
+  if (digit < 4) {
+    if (value < 16) {
+      // blank all       
+      for (int i=0; i<4; i++) {
+        digitalWrite(anodepins[i],LOW);
+      }
+
+      if (digit==3) (digitalWrite(dp,LOW));
+      else (digitalWrite(dp,HIGH));
+
+      for (int i=0; i<4; i++) {
+        digitalWrite(ls247pins[i],(bitRead(value,i)));
+      } 
+    }
+    // turn on the appropriate digit
+    digitalWrite(anodepins[digit],HIGH);
+  }
+  delay(1);
+}
+
+///////////////
+// INTERRUPT //
+///////////////
+
+void doEncoder() {
+  // If pinA and pinB are both high or both low, it is spinning
+  // forward. If they're different, it's going backward.
+  if (digitalRead(encoderPinA) == digitalRead(encoderPinB)) {
+    frequency -= incrFM; 
+    delay(200);
+    frequency = constrain( frequency, botFM, topFM);  // Keeps us in range
+  } 
+  else {
+    frequency += incrFM; 
+    delay(200);
+    frequency = constrain( frequency, botFM, topFM);  // Keeps us in range
+  }
+}
+
+/////////////////////////////
+// RADIO CONTROL FUNCTIONS //
+/////////////////////////////
 
 void transmitter_setup(long initFrequency) {
   i2c_send(0x0E, B00000101); //Software reset
@@ -219,52 +278,31 @@ void i2c_send(byte reg, byte data) {
   delay(5);                                       // allow register to set
 }
 
-void checkSerial() {
-  if (Serial.available() > 0) {
-    int inByte = Serial.read(); // read incoming message
-    // set transmitter into standby mode
-    if (inByte == 254) transmitter_standby(frequency);
-    // change the transmission frequency
-    if (inByte == 255) serialCount = 0;
-    // set incoming byte into a temporary array and move through it
-    // these values will be reassigned
-    else {
-      serialArray[serialCount] = inByte;
-      serialCount++; 
-      if (serialCount == 2) {
-        transmitter_standby(frequency);
-        int left = serialArray[0] * 100;
-        long tempFrequency = left + (long) serialArray[1];
-        frequency = tempFrequency * 10000;
-        set_freq(frequency);
-        saveFrequency(frequency); // save the Frequency to EEPROM Memory
-        // write the incoming messages for debugging in Max
-        Serial.write(serialArray[0]);
-        Serial.write(serialArray[1]);
-        // reset the serial count to receive the next message
-        serialCount = 0;
-      }
-    }
-  }
+//////////////////////
+// EEPROM FUNCTIONS //
+//////////////////////
+
+void saveFrequency(long aFrequency) {
+  long memFrequency = 0;                   // for use in Read / Write to EEProm
+
+  //Serial.print( "Saving: " );
+  //Serial.println(aFrequency, DEC);
+
+  memFrequency = aFrequency / 10000;
+  EEPROM.write(0, memFrequency / 256);   // right-most byte
+  EEPROM.write(1, memFrequency - (memFrequency / 256) * 256);   // next to right-most byte
 }
 
-///////////////
-// INTERRUPT //
-///////////////
+long loadFrequency() {
+  long memFrequency = 0;                   // for use in Read / Write to EEProm
 
-void doEncoder() {
-  // If pinA and pinB are both high or both low, it is spinning
-  // forward. If they're different, it's going backward.
-  if (digitalRead(encoderPinA) == digitalRead(encoderPinB)) {
-    frequency -= incrFM; 
-    delay(200);
-    frequency = constrain( frequency, botFM, topFM);  // Keeps us in range
-  } 
-  else {
-    frequency += incrFM; 
-    delay(200);
-    frequency = constrain( frequency, botFM, topFM);  // Keeps us in range
-  }
+  memFrequency = EEPROM.read(0) * 256 + EEPROM.read(1);
+  memFrequency *= 10000;
+
+  //Serial.print("Retrieving: " );
+  //Serial.println(memFrequency, DEC);
+  return memFrequency;
 }
+
 
 
